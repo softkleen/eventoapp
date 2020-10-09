8a00b046a0d4af76b54dd02ac92a7de591267ce2
Wellington Santos <wellington.vsantos@gmail.com>
2020-10-09 11:55:50 -0300

    criação service api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from 'src/services/api.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,11 +17,13 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    ApiService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
---
+++ src/services/api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+server:string  = "https://softkleen.com.br/web/api/";
+  constructor(private http: HttpClient ) { }
+
+connectApi(dados:any, nomeApi:string){
+  const httpOptions = {
+    headers: new HttpHeaders({'Content-Type':'application/json'})
+  };
+  let url = this.server + nomeApi;
+  return this.http.post(url, JSON.stringify(dados), httpOptions).map(res=>res);
+}
+
+}
