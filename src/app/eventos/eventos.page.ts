880f4a1cafd923b0d63f05df0dd01923e9fb5833
Wellington Santos <wellington.vsantos@gmail.com>
2020-10-13 11:55:36 -0300

    Listando eventos

--- src/app/eventos/eventos.page.ts
+++ src/app/eventos/eventos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/services/api.service';
 
 @Component({
   selector: 'app-eventos',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.page.scss'],
 })
 export class EventosPage implements OnInit {
+eventos:any[] = [];
+nome:string="";
 
-  constructor() { }
+  constructor(private service : ApiService) { }
 
   ngOnInit() {
   }
-
+  ionViewWillEnter(){
+    this.carregar();
+  }
+  carregar(){
+    return new Promise(ret =>{
+      this.eventos = [];
+      let dados = {
+        requisicao:"listar",
+        nome:""
+      };
+      this.service.connectApi(dados,"evento.php").subscribe(data =>{
+        if(data["result"]=='0'){
+          this.ionViewWillEnter();
+        }else{
+          for (let evento of data['result']){
+            this.eventos.push(evento);
+          }
+        }
+        //debugger;
+    });
+   });
+}
 }
